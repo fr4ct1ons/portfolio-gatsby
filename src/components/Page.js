83bcf3e448c5402ecc871e0fb094aaa1a4779f94
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2021-07-13 23:50:23 -0300

    adding font?

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //import './App.css';
-import '../styles/Navbar.css';
+//import '../styles/Navbar.css';
+import '../styles/global.css';
 
 class Page extends React.Component{
 
@@ -9,7 +10,7 @@ render(){
 return(
     <div style={{width: "70%"}, {textAlign: "center"}}>
         <div style={{width: "66%"}, {margin: "auto"}, {display: "block"} }>
-            <h1 style={{color: "red"}}>{this.props.title}</h1>
+            <h1 style={{color: "red"}, {fontFamily: "Arimo"}}>{this.props.title}</h1>
             <h2 style={{color: "rgb(150, 150, 150)"}}><i>{this.props.subtitle}</i></h2>
         </div>
     </div>
