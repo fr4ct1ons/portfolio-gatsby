5044e644f229f1bc289a5bed29e5eebff6c900ff
fr4ct1ons-lucena <gabriellucena23@gmail.com>
2021-11-03 22:38:40 -0300

    Added gallery on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,24 @@ import Page from "../components/Page"
 import Layout from "../components/layout"
 import "../fonts/Arimo-Regular.ttf"
 import GalleryEntry from "../components/GalleryEntry"
-
+import artworks from "../../public/images/ArtWorks.png"
+import software from "../../public/images/CSRP.png"
+import {Link} from 'gatsby';
 
 export default function Home() {
   return<Layout>
     <Page title = "WHO AM I" subtitle = "Introducing who I am, what I work with and my goals"></Page>
     <div className="TextBlock">
-      <p>Hey there! My name is Gabriel Lucena, also known as FR4CT1ONS.</p>
-      <p>I am a tech artist with an ongoing bachelor's undergraduation program at Universidade Federal do Rio Grande do Norte.</p>
-      <p>I am experient in Unity, Unreal, Blender and Substance Painter</p>
+      <p>Hi there! I'm Gabriel Lucena, also known as FR4CT1ONS. I'm a brazilian game developer and tech artist.</p>
+      <p>I have a bachelor's degree in information technology with experience with 2D and 3D games with Unity and Unreal, as well as with tools such as Blender and Substance Painter.</p>
+      <p>Below you can check out some of my past works, such as 2D and 3D artworks including visual effects, games I worked on and other software I made, like tools and other programs.</p>
     </div>
     <div>
-      <GalleryEntry url="facebook.com" title="Test" img="https://pro2-bar-s3-cdn-cf6.myportfolio.com/2e727ec7-f6e9-4fd4-b97f-93196e1208ad/72bce5f9-2596-4820-b1b7-dd2a0bd7d964.jpg?h=9446e874cc19b1da82695f7fc0dc722c"/>
-{/*
+    <GalleryEntry url="art" title="ART WORKS" img={artworks}/>
+    <GalleryEntry url="games" title="GAMES" img="https://pro2-bar-s3-cdn-cf6.myportfolio.com/2e727ec7-f6e9-4fd4-b97f-93196e1208ad/72bce5f9-2596-4820-b1b7-dd2a0bd7d964.jpg?h=9446e874cc19b1da82695f7fc0dc722c"/>
+    <GalleryEntry url="games" title="SOFTWARE" img={software}/>{/*
+    {/*
+    <GalleryEntry url="games" title="GAMES" img="https://pro2-bar-s3-cdn-cf6.myportfolio.com/2e727ec7-f6e9-4fd4-b97f-93196e1208ad/72bce5f9-2596-4820-b1b7-dd2a0bd7d964.jpg?h=9446e874cc19b1da82695f7fc0dc722c"/>{/*
       <div className="inlineDiv">
         <h3>TOOLS</h3>
           <ul>
@@ -42,5 +47,7 @@ export default function Home() {
       </div>
 */}
     </div>
+
+    <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer" style={{color: "rgb(75, 75, 75)"}}>Made with Gatsby.js</a>
   </Layout>
 }
