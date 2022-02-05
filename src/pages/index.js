ec0cb0f1c88384af7f94c616204694d0923b697a
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2022-02-05 18:19:30 -0300

    Added work experiencecs

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import {Link} from 'gatsby';
 import SkillItem from "../components/SkillItem"
 
 import "../styles/education.css"
+import "../styles/experience.css"
 
 export default function Home() {
   return<Layout>
@@ -40,7 +41,9 @@ export default function Home() {
           <Link to="https://gamejolt.com/@fr4ct1ons"><StaticImage src="../images/socialMedia/gamejolt-icon.png"/></Link>
         </div>
       </div>
+      </div>
       <h1>TOOLS AND SKILLS</h1>
+      <h2>You can hover over some of these icons to see their names</h2>
       <Skills style={{width: '70%'}} title="I am highly skilled at">
       <SkillItem size='10%' img="Unity.png" name="Unity"/>
       <SkillItem size='10%' img="Unreal.png" name="Unreal"/>
@@ -89,6 +92,7 @@ export default function Home() {
         </ul>
       </div>
       <h1>EDUCATION</h1>
+      <h2>Info about my professional formation</h2>
       <div className="EducationItem">
         <StaticImage src="../images/logos/UFRN.png"/>
         <div className="Text">
@@ -106,7 +110,36 @@ export default function Home() {
         <h3>2020</h3>
         </div>
       </div>
-    </div>
+
+      <h1>EXPERIENCE</h1>
+      <h2>Previous work experiences</h2>
+      <div className="ExperienceItem">
+        <StaticImage src="../images/logos/Stegosoft.png" height={250}/>
+        <div className="Text">
+          <h1>VFX Artist</h1>
+          <h2>Stegosoft Games | 2021</h2>
+          <p>Production of battle visual effects for attacks and buffs, as well as scenery props and environmental effects such as rain, falling leafs and birds, for Rise of the Third Power.</p>
+        </div>
+      </div>
+
+      <div className="ExperienceItem">
+        <StaticImage src="../images/logos/MaleDoll.png" height={250}/>
+        <div className="Text">
+          <h1>Intern Unity Programmer</h1>
+          <h2>Male Doll | 2020</h2>
+          <p>Responsible for the development of player controllers, AI programming and scenery elements for Boyfriend's Rescue and editor tools, UI and gameplay for the game Bring me a man, Santa</p>
+        </div>
+      </div>
+
+      <div className="ExperienceItem">
+        <StaticImage src="../images/logos/Baladeira.png" height={250}/>
+        <div className="Text">
+          <h1>Tech Artist</h1>
+          <h2>Baladeira Studio | 2019 - 2022</h2>
+          <p>Baladeira Studio is a studio I created with a few friends in 2019, after winning third place at Natal Game Jam 2019. I was responsible for production of visual effects & shaders, 3D models and tool development for the game "1498". I also programmed AI and gameplay for "Poço Frio".</p>
+        </div>
+      </div>
+
 
   </Layout>
 }
